6dc59620f527cef2448088a3ee29444dbdbc701c
Fisherman August <fshrmn.august@gmail.com>
2025-06-22 12:13:49 +0300

    string as byte array

--- main.go
+++ main.go
@@ -3,17 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	planets := [...]string{
-		"Меркурий",
-		"Венера",
-		"Земля",
-		"Марс",
-		"Юпитер",
-		"Сатурн",
-		"Уран",
-		"Нептун",
-	}
-	terr := planets[4:]
-	fmt.Println(terr)
+	question := "¿Cómo estás?"
+	fmt.Println(question[:6])
 
 }
