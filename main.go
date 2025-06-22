7ccffc5b7f96d7ab18ebceed66d51b9ced9822e7
Fisherman August <fshrmn.august@gmail.com>
2025-06-22 13:08:14 +0300

    finish

--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 )
 
 func hyperspace(worlds []string) {
-	//worlds []string - ссылка на тот же самый массив, что и []string{" Венера ", "Земля", " Марс"}
 	for i := range worlds {
 		worlds[i] = strings.TrimSpace(worlds[i])
 	}
 }
 
 func main() {
-	planets := []string{" Венера ", "Земля", " Марс"} // где-то в память есть оригианльный массив, на который этот слайс ссылается
-	hyperspace(planets)
+	// planets := []string{" Венера ", "Земля", " Марс"}
+	// hyperspace(planets)
 
-	planets2 := []string{" Нептун ", "  Плутон "}
-	hyperspace(planets2)
+	// planets2 := []string{" Нептун ", "  Плутон "}
+	// hyperspace(planets2)
 
-	fmt.Println(strings.Join(planets, ""))
-	fmt.Println(strings.Join(planets2, ""))
-	fmt.Printf("%p", &planets)
+	// fmt.Println(strings.Join(planets, ""))
+	// fmt.Println(strings.Join(planets2, ""))
+	// fmt.Printf("%T", &planets)
 }
