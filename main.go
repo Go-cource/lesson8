6491f74a2f20484b42df1d4e580c839d6429942d
Fisherman August <fshrmn.august@gmail.com>
2025-06-22 10:37:39 +0300

    init git with planets

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var planets [8]string
+	planets[0] = "Меркурий"
+	planets[1] = "Венера"
+	planets[2] = "Земля"
+
+	earth := planets[2]
+	fmt.Println(planets)
+	fmt.Println("Наша планета: ", earth)
+
+	fmt.Println(len(planets))
+	fmt.Println(planets[5] == "") //elem with index 5 - ""? true
+
+}
