e21a515e8ec486901773d51ddbffa4d41a3d81e2
Fisherman August <fshrmn.august@gmail.com>
2025-06-22 12:38:16 +0300

    slices

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ func main() {
 
 	// symb := "ががc"
 	// fmt.Println(symb[:7])
-	
 
+	// ewq := [3]string{"qwe", "ewq", "asd"}
+	// qwe := []string{"Церера", "Плутон", "Хаумеа", "Макемаке", "Эрида"}
+
+	// fmt.Printf("%T\n", ewq)
+	// fmt.Printf("%T", qwe)
 
 }
